abc98fda74efab9f64819b6c633bb301ecf2a95b
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-07-10 10:57:34 +0200

    Test Factory

---
+++ Assets/_Script/CNpcSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CNpcSpawner : MonoBehaviour
+{
+    IHuman humanBeggar;
+    IAnimal AnimalCat;
+
+    //Factory Human
+    public CHumanFactroy humanFactroy=default;
+
+    //Factory Animal
+    public CAnimalFactory animalFactory=default;
+
+    //Test Factory NPC
+    public void SpwanNpc()
+    {
+        if(humanFactroy && animalFactory)
+        {
+            humanBeggar = humanFactroy.GetHuman(NPCHumanType.Beggar);
+
+            AnimalCat = animalFactory.GetAnimal(NPCAnimalType.Cat);
+
+            humanBeggar.Speak();
+
+            AnimalCat.Voice();
+
+        }
+
+        return;
+
+    }
+
+    public void Start()
+    {
+        SpwanNpc();
+    }
+}
